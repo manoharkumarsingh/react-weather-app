da61e0b1a775754f0c49fa650475bc8b8b51de4f
Manohar Singh <manoharsingh@Manohars-MacBook-Pro.local>
2024-03-21 11:55:52 +0530

    Initial Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "260cb335441aa8e8bed891ff699b8837",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
